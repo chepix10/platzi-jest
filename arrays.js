45327be32a741992e9a2745904c839c653d428bf
David Salas <jlmruiz@outlook.com>
2019-08-05 15:12:02 -0600

    Added code up to v. 9

---
+++ arrays.js
@@ -0,0 +1,7 @@
+const fruits = ['banana', 'melon', 'sandia', 'naranja', 'limon', 'pepino']
+const colors = ['azul', 'verde', 'rojo', 'rosa', 'amarillo']
+
+export {
+  fruits,
+  colors
+}
---
+++ tests/arrays.test.js
@@ -0,0 +1,13 @@
+import { fruits, colors } from '../arrays'
+
+describe('Comprobamos que existe un elemento', () => {
+  test('¿Contiene una banana?', () => {
+    expect(fruits).toContain('banana')
+  })
+  test('¿Contiene un plátano?', () => {
+    expect(fruits).not.toContain('platano')
+  })
+  test('Tamaño de un arreglo', () => {
+    expect(fruits).toHaveLength(6)
+  })
+})
