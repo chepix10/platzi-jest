7400d839d4390a834e35350243d2f7e823342721
David Salas <jlmruiz@outlook.com>
2019-08-05 15:19:44 -0600

    Added code unitl v. 10

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
-const fruits = ['banana', 'melon', 'sandia', 'naranja', 'limon', 'pepino']
-const colors = ['azul', 'verde', 'rojo', 'rosa', 'amarillo']
+const fruits = () => ['banana', 'melon', 'sandia', 'naranja', 'limon', 'pepino']
+const colors = () => ['azul', 'verde', 'rojo', 'rosa', 'amarillo']
 
 export {
   fruits,
--- tests/arrays.test.js
+++ tests/arrays.test.js
@@ -2,12 +2,15 @@ import { fruits, colors } from '../arrays'
 
 describe('Comprobamos que existe un elemento', () => {
   test('¿Contiene una banana?', () => {
-    expect(fruits).toContain('banana')
+    expect(fruits()).toContain('banana')
   })
   test('¿Contiene un plátano?', () => {
-    expect(fruits).not.toContain('platano')
+    expect(fruits()).not.toContain('platano')
   })
   test('Tamaño de un arreglo', () => {
-    expect(fruits).toHaveLength(6)
+    expect(fruits()).toHaveLength(6)
+  })
+  test('Comprobaremos que existe un color', () => {
+    expect(colors()).toContain('azul')
   })
 })
