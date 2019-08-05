03b72332c2aaf0ba982a43526eaa4ef6aa028510
David Salas <dsalas@digitalgeko.com>
2019-08-05 11:36:12 -0600

    Added code until v. 5

---
+++ tests/maths.test.js
@@ -0,0 +1,16 @@
+import { sumar, restar, multiplicar, dividir } from '../maths'
+
+describe('Cálculos matemáticos', () => {
+  test('Sumar', () => {
+    expect(sumar(1, 1)).toBe(2)
+  })
+  test('Multiplicar', () => {
+    expect(multiplicar(2,2)).toBe(4)
+  })
+  test('Restar', () => {
+    expect(restar(1,1)).toBe(0)
+  })
+  test('Dividir', () => {
+    expect(dividir(2,2)).toBe(1)
+  })
+})
