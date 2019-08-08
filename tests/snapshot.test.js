fc2fb5c4b87b00aa254a88be36495db6cadea981
David Salas <jlmruiz@outlook.com>
2019-08-08 15:19:57 -0600

    Added code until v. 20

--- tests/snapshot.test.js
+++ tests/snapshot.test.js
@@ -5,4 +5,22 @@ describe('Es hora de las instantáneas', () => {
   test('Snapshot', () => {
     expect(getCharacter(rick)).toMatchSnapshot()
   })
+
+  test('Siempre fallará la instantánea', () => {
+    const user = {
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 1000000)
+    }
+    expect(user).toMatchSnapshot()
+  })
+
+  test('Tenemos una excepción', () => {
+    const user = {
+      id: Math.floor(Math.random() * 1000000),
+      name: 'Oscar Barajas'
+    }
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number)
+    })
+  })
 })
