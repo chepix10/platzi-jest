a58c8242818f2606b8889e2fe179bf316d208be9
David Salas <jlmruiz@outlook.com>
2019-08-08 14:20:03 -0600

    Added code and dependencies up to v. 17

---
+++ tests/async.test.js
@@ -0,0 +1,18 @@
+import { getDataFromApi } from '../promise'
+
+describe('Probar Async/Await', () => {
+  test('Realizar una petición a una api', async () => {
+    const api = 'https://rickandmortyapi.com/api/character'
+
+    await getDataFromApi(api).then(data => {
+      expect(data.results.length).toBeGreaterThan(0)
+    })
+  })
+
+  test('Realizar una petición a una api con id', async () => {
+    const getRick = 'https://rickandmortyapi.com/api/character/1'
+    await getDataFromApi(getRick).then(data => {
+      expect(data.name).toEqual('Rick Sanchez')
+    })
+  })
+})
